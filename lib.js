ec31048357beab84bd42c7dee00502fd06dd5764
ctianl <570023481@qq.com>
2019-12-05 17:21:36 +0800

    Update lib.js

--- lib.js
+++ lib.js
@@ -1,4 +1,15 @@
-let a=100
-let b="fuckyou"
-let c=()=>{console.log(120)}
-export {a,b,c}
+const name="ctianl"
+const age=22
+class Person{
+    constructor(name,age){
+        this.name=name
+        this.age=age
+    }
+    hw(){
+        console.log("Hello,world!")
+    }
+    static eat(){
+        console.log("This person is eating something.")
+    }
+}
+export {name,age,Person}
