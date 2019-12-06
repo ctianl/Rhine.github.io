434acac789b57c9141c82907d63590fc7569fa4d
ctianl <570023481@qq.com>
2019-12-06 15:07:21 +0800

    Update lib.js

--- lib.js
+++ lib.js
@@ -1,15 +1,4 @@
-const name="ctianl"
-const age=22
-class Person{
-    constructor(name,age){
-        this.name=name
-        this.age=age
-    }
-    hw(){
-        console.log("Hello,world!")
-    }
-    static eat(){
-        console.log("This person is eating something.")
-    }
-}
-export {name,age,Person}
+let a=10
+let b="fuck you baby"
+let c=()=>{console.log("引入的模块的内容")}
+export {a as num,b as str,c as fn}
