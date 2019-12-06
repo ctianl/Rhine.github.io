aee454c26b51ad128c6687566d7c90b94744df00
ctianl <570023481@qq.com>
2019-12-06 15:16:28 +0800

    Update lib.js

--- lib.js
+++ lib.js
@@ -1,10 +1,5 @@
-// let a=10
-// let b="fuck you baby"
-// let c=()=>{console.log("引入的模块的内容")}
-// export {a as num,b as str,c as fn}
-
 //可以直接写函数和变量
-export let fn=()=>{console.log("Hello,baby!")},bar=100
+export let fn=()=>{console.log("Hello,baby!")}
 //默认只输出一个功能
 
 //只输出一个功能，匿名
